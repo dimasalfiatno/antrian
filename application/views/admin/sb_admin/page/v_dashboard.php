7f14a56c54f319b707bfd352aa6590aa06f99da0
dimas <dimas.alfiatno95@gmail.com>
2020-11-11 15:32:18 +0700

    awal

---
+++ application/views/admin/sb_admin/page/v_dashboard.php
@@ -0,0 +1,147 @@
+<!-- Morris Charts CSS -->
+<link href="css/plugins/morris.css" rel="stylesheet">
+<div class="row">
+    <div class="col-lg-3 col-md-6">
+        <div class="panel panel-primary">
+            <div class="panel-heading">
+                <div class="row">
+                    <div class="col-xs-3">
+                        <!-- <i class="fa fa-comments fa-5x"></i> -->
+                    </div>
+                    <div class="col-xs-9 text-right">
+                        <div class="huge"><?php echo $poli_umum?></div>
+                        <div>Poli Umum</div>
+                    </div>
+                </div>
+            </div>
+            <!-- <a href="#"> -->
+                <div class="panel-footer">
+                    <!-- <span class="pull-left">View Details</span>
+                    <span class="pull-right"><i class="fa fa-arrow-circle-right"></i></span> -->
+                    <div class="clearfix"></div>
+                </div>
+            </a>
+        </div>
+    </div>
+    <div class="col-lg-3 col-md-6">
+        <div class="panel panel-green">
+            <div class="panel-heading">
+                <div class="row">
+                    <div class="col-xs-3">
+                        <!-- <i class="fa fa-tasks fa-5x"></i> -->
+                    </div>
+                    <div class="col-xs-9 text-right">
+                        <div class="huge"><?php echo $poli_gigi?></div>
+                        <div>Poli Gigi</div>
+                    </div>
+                </div>
+            </div>
+            <!-- <a href="#"> -->
+                <div class="panel-footer">
+                    <!-- <span class="pull-left">View Details</span>
+                    <span class="pull-right"><i class="fa fa-arrow-circle-right"></i></span> -->
+                    <div class="clearfix"></div>
+                </div>
+            </a>
+        </div>
+    </div>
+    <div class="col-lg-3 col-md-6">
+        <div class="panel panel-yellow">
+            <div class="panel-heading">
+                <div class="row">
+                    <div class="col-xs-3">
+                       <!--  <i class="fa fa-shopping-cart fa-5x"></i> -->
+                    </div>
+                    <div class="col-xs-9 text-right">
+                        <div class="huge"><?php echo $poli_anak?></div>
+                        <div>Poli Imunisasi</div>
+                    </div>
+                </div>
+            </div>
+            <!-- <a href="#"> -->
+                <div class="panel-footer">
+                   <!--  <span class="pull-left">View Details</span>
+                    <span class="pull-right"><i class="fa fa-arrow-circle-right"></i></span> -->
+                    <div class="clearfix"></div>
+                </div>
+            </a>
+        </div>
+    </div>
+    <div class="col-lg-3 col-md-6">
+        <div class="panel panel-red">
+            <div class="panel-heading">
+                <div class="row">
+                    <div class="col-xs-3">
+                        <!-- <i class="fa fa-support fa-5x"></i> -->
+                    </div>
+                    <div class="col-xs-9 text-right">
+                        <div class="huge"><?php echo $poli_bedah?></div>
+                        <div>Poli Bedah</div>
+                    </div>
+                </div>
+            </div>
+            <!-- <a href="#"> -->
+                <div class="panel-footer">
+                    <!-- <span class="pull-left">View Details</span>
+                    <span class="pull-right"><i class="fa fa-arrow-circle-right"></i></span> -->
+                    <div class="clearfix"></div>
+                </div>
+            </a>
+        </div>
+    </div>
+    <div class="col-lg-3 col-md-6">
+        <div class="panel panel-primary">
+            <div class="panel-heading">
+                <div class="row">
+                    <div class="col-xs-3">
+                        <!-- <i class="fa fa-support fa-5x"></i> -->
+                    </div>
+                    <div class="col-xs-9 text-right">
+                        <div class="huge"><?php echo $poli_penyakit_dalam?></div>
+                        <div>Poli Penyakit Dalam</div>
+                    </div>
+                </div>
+            </div>
+            <!-- <a href="#"> -->
+                <div class="panel-footer">
+                    <!-- <span class="pull-left">View Details</span>
+                    <span class="pull-right"><i class="fa fa-arrow-circle-right"></i></span> -->
+                    <div class="clearfix"></div>
+                </div>
+            </a>
+        </div>
+    </div>
+    <div class="col-lg-3 col-md-6">
+        <div class="panel panel-green">
+            <div class="panel-heading">
+                <div class="row">
+                    <div class="col-xs-3">
+                        <!-- <i class="fa fa-support fa-5x"></i> -->
+                    </div>
+                    <div class="col-xs-9 text-right">
+                        <div class="huge"><?php echo $poli_kandungan?></div>
+                        <div>Poli Kandungan</div>
+                    </div>
+                </div>
+            </div>
+            <a href="#">
+                <div class="panel-footer">
+                    <!-- <span class="pull-left">View Details</span>
+                    <span class="pull-right"><i class="fa fa-arrow-circle-right"></i></span> -->
+                    <div class="clearfix"></div>
+                </div>
+            </a>
+        </div>
+    </div>
+</div>
+<!-- /.row -->
+
+
+
+
+
+<script src="js/jquery.js"></script>
+<!-- Morris Charts JavaScript -->
+<script src="js/plugins/morris/raphael.min.js"></script>
+<script src="js/plugins/morris/morris.min.js"></script>
+<script src="js/plugins/morris/morris-data.js"></script>
